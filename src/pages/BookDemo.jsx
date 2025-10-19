41fb6fa2d443f94bd0615896a3c735b745a3bc25
Old User <sanjana.b0831@gmail.com>
2025-10-19 09:54:54 +0530

    Added vercel and chnaged css and scroll

--- src/pages/BookDemo.jsx
+++ src/pages/BookDemo.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useState } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from "@emailjs/browser";
 
 // Define the two main "courses" (Student Demo and Tutor Application)
 const TABS = {
